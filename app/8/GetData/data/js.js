b88d32bd7030e89d728e13be4224a3be9f688608
Vitaliy Zhyrytskyy <vitaliy.zhyrytskyy@epam.com>
2016-04-21 16:16:59 +0300

    Start Point

---
+++ app/8/GetData/data/js.js
@@ -0,0 +1,3 @@
+function myAlert(){
+	alert('myAlert function');
+}
---
+++ app/8/GetData/script.js
@@ -0,0 +1,29 @@
+$(function(){
+
+	
+
+	
+
+	// load xml	
+	$("#b1").on("click", function(){
+		
+	});
+	
+	// load html	
+	$("#b2").on("click", function(){
+		
+	});
+	
+	// load json	
+	$("#b3").on("click", function(){
+		
+	});
+	
+	// run script
+	$("#b4").on("click" ,function(){
+		
+	});
+
+	
+
+})
