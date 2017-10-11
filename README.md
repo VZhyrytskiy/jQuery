a2b7f61473ccef5fafdc4649fd1005a61e5e216a
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-10-11 13:23:16 +0300

    change the path to the lib

--- README.md
+++ README.md
@@ -3,13 +3,9 @@ Setup Environment
 1. install node.js from https://nodejs.org/en/
 
 2. install git from https://git-scm.com/
-	See options in git-option.png
 
-3. install bower from command line: 
-	> npm install -g bower
+3. install gulp from command line:
+    > npm install -g gulp
 
-4. install gulp from command line: 
-	> npm install -g gulp
-
-5. run project from project folder:
-	> npm start
+4. run project from project folder:
+    > npm start
--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,35 @@ var gulp = require("gulp");
 var plug = require("gulp-load-plugins")();
 
 var source = [
-		'app/**/*.js', 
-		"!app/vendor/**/*.*"
-	];
+        'app/**/*.js',
+        "!app/vendor/**/*.*"
+    ];
 
 gulp.task('webserver', function() {
-	return gulp
-		.src('app')										// root
-		.pipe(plug.webserver({
-			livereload: true,							// livereload
-			directoryListing: true,
-			// to open page type:
-			// http://localhost:8000/1/01_BasicSelectors.html 
-			open: "http://localhost:8000/index.html"
-	}));
+    return gulp
+        .src('.')                                        // root
+        .pipe(plug.webserver({
+            livereload: true,                            // livereload
+            directoryListing: true,
+            // to open page type:
+            // http://localhost:8000/1/01_BasicSelectors.html
+            open: "http://localhost:8000/index.html"
+    }));
 });
 
 gulp.task("watch", function() {
-	return gulp
-		.watch(source)
-		.on("change", function(event) {
-			console.log("*** File " + event.path + " was " + event.type + ", running tasks...");
-		});
+    return gulp
+        .watch(source)
+        .on("change", function(event) {
+            console.log("*** File " + event.path + " was " + event.type + ", running tasks...");
+        });
 });
 
 // The default task is 'webserver'
 gulp.task("default", ["webserver", "watch"]);
 
-/*  
-	if you don't use the default name if gulpfile.js,
-	run gulp like this:
-	gulp --gulpfile youFileName taskname
+/*
+    if you don't use the default name if gulpfile.js,
+    run gulp like this:
+    gulp --gulpfile youFileName taskname
 */
