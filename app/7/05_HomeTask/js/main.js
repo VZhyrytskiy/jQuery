953173f79cac94672881a6ddc4632d3ef87f6702
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-10-13 12:53:29 +0300

    updated run functions

--- app/7/05_HomeTask/js/main.js
+++ app/7/05_HomeTask/js/main.js
@@ -1,29 +1,29 @@
-$(document).ready(function(){
+$(function(){
 
-	// увеличение маленьких фото
-	$(".small_photo").on("click", function(){
-		// снять со всех класс выделение
-		$(".small_photo").removeClass("small_photo_selected");
-		var photo_url = $(this).addClass("small_photo_selected").attr("src");
-		var new_photo_url = photo_url.replace("/s/", "/m/");
-		var new_a_url = photo_url.replace("/s/", "/l/");
-		$(".photo").attr("src", new_photo_url)
-				   .parent().attr("href", new_a_url);
-	});
-	
-	// делаем ссылку на корзину, когда она не пуста
-	if ($("#basket").has("#basket_empty.hidden").length) {
-		$("#basket").css("cursor", "pointer")
-					.attr("title", "Посмотреть содержимое корзины")
-					.on("click", function(){
-						window.location.href="#";
-					});
-	}
-	// убираем ссылку, когда корзина пуста
-	else {
-		$("#basket").css("cursor", "default")
-					.removeAttr("title")
-					.off("click");
-	}
+    // увеличение маленьких фото
+    $(".small_photo").on("click", function(){
+        // снять со всех класс выделение
+        $(".small_photo").removeClass("small_photo_selected");
+        var photo_url = $(this).addClass("small_photo_selected").attr("src");
+        var new_photo_url = photo_url.replace("/s/", "/m/");
+        var new_a_url = photo_url.replace("/s/", "/l/");
+        $(".photo").attr("src", new_photo_url)
+                   .parent().attr("href", new_a_url);
+    });
+
+    // делаем ссылку на корзину, когда она не пуста
+    if ($("#basket").has("#basket_empty.hidden").length) {
+        $("#basket").css("cursor", "pointer")
+                    .attr("title", "Посмотреть содержимое корзины")
+                    .on("click", function(){
+                        window.location.href="#";
+                    });
+    }
+    // убираем ссылку, когда корзина пуста
+    else {
+        $("#basket").css("cursor", "default")
+                    .removeAttr("title")
+                    .off("click");
+    }
 
 });
--- app/7/05_HomeTask/js/setEqual.js
+++ app/7/05_HomeTask/js/setEqual.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-	// установка одинаковой высоты div
-	function setEqualHeight(columns) {
-	var tallestcolumn = 0;
-	columns.each(function(){
-				currentHeight = $(this).height();
-				//currentHeight = $(this).outerHeight(true);
-				if(currentHeight > tallestcolumn){
-					tallestcolumn  = currentHeight;
-				}
-			});
-	columns.height(tallestcolumn);
-	}
-	setEqualHeight($("#container, #left, #right"));
+$(function(){
+    // установка одинаковой высоты div
+    function setEqualHeight(columns) {
+    var tallestcolumn = 0;
+    columns.each(function(){
+                currentHeight = $(this).height();
+                //currentHeight = $(this).outerHeight(true);
+                if(currentHeight > tallestcolumn){
+                    tallestcolumn  = currentHeight;
+                }
+            });
+    columns.height(tallestcolumn);
+    }
+    setEqualHeight($("#container, #left, #right"));
 });
